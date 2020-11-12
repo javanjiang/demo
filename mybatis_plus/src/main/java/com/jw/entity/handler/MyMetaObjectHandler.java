1504b0d9f48c2508dab7381df72c1d4526cc7971
javan <javanjiang@163.com>
2020-11-12 16:35:30 +0800

    1.0.0

---
+++ mybatis_plus/src/main/java/com/jw/entity/handler/MyMetaObjectHandler.java
@@ -0,0 +1,20 @@
+package com.jw.entity.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+
+/*自动填充
+* 实现MetaObjectHandler
+* */
+
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
